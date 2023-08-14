5af4bed9c148fc7dfe08693c85352fa354378fde
Jia-Yao Zhao <63852433+zjy040525@users.noreply.github.com>
2023-08-14 14:06:05 +0800

    fix: test node env merge

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,6 +3,7 @@ import { FastifyPluginAsync } from 'fastify';
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.all('/', async (_request, reply) => {
     reply.code(404);
+    return;
   });
 };
 
